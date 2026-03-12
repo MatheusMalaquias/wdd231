c0872176c4a23bbcc698e16ab3c250e9a87b3951
Your Name <you@your-domain.com>
2026-03-12 19:54:36 -0300

    Updated

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,55 @@
+const url = "data/members.json";
+const membersContainer = document.querySelector("#members");
+
+async function getMembers() {
+
+const response = await fetch(url);
+
+const data = await response.json();
+
+displayMembers(data);
+
+}
+
+function displayMembers(members){
+
+members.forEach(member => {
+
+let card = document.createElement("section");
+
+card.innerHTML = `
+<img src="images/${member.image}" alt="${member.name}">
+<h3>${member.name}</h3>
+<p>${member.address}</p>
+<p>${member.phone}</p>
+<a href="${member.website}" target="_blank">Visit Website</a>
+`;
+
+membersContainer.appendChild(card);
+
+});
+
+}
+
+getMembers();
+
+
+const gridButton = document.querySelector("#grid");
+
+const listButton = document.querySelector("#list");
+
+gridButton.addEventListener("click", () => {
+
+membersContainer.classList.add("grid");
+
+membersContainer.classList.remove("list");
+
+});
+
+listButton.addEventListener("click", () => {
+
+membersContainer.classList.add("list");
+
+membersContainer.classList.remove("grid");
+
+});
